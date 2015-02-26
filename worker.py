432042e5e7090a2df349e3058749c636cd653104
Spencer Krum <nibz@spencerkrum.com>
2015-02-26 10:21:13 -0800

    manually use vagrant to destroy

--- worker.py
+++ worker.py
@@ -64,8 +64,9 @@ def write_log(work_item, response):
 
 def clean_up(tempdir):
     time.sleep(120) # give vagrant time to self-kill
-    jobdir = (tempdir + "/job/.vagrant/beaker_vagrant_files/")
+    jobdir = (tempdir + "/job/.vagrant/beaker_vagrant_files/default.yml")
     vagrant = subprocess.Popen(["vagrant", "destroy"], cwd=jobdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=runenv)
+    vagrant.communicate()
 
     clean_tempdir(tempdir)
 
