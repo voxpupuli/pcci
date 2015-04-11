e785074f8f625bdcf3b65916d5c7af178f71d1ba
Spencer Krum <nibz@spencerkrum.com>
2015-04-10 22:04:15 -0700

    second draft of run_test

---
+++ run_test.py
@@ -0,0 +1,148 @@
+import datetime
+import os
+import shutil
+import signal
+import subprocess
+import sys
+import tempfile
+import time
+
+import redis
+
+import config
+
+
+def setup_worker():
+    #register as a worker
+    workers = r.get('workers')
+    if workers is None:
+        r.set('workers', 0)
+    else:
+        print "found {0} workers".format(workers)
+        if int(workers) >= config.workers:
+            # we already have enough workers
+            print "deactivating because we already have enough workers"
+            sys.exit()
+    r.incr('workers')
+
+
+def signal_handler(signal, frame):
+    print('Shutting down worker')
+    r.decr('workers')
+    sys.exit(0)
+
+
+def create_pr_env(work_item):
+    print "working on {0}".format(work_item)
+    org, project, pr = work_item.split('/')
+    tempdir = tempfile.mkdtemp()
+    git_clone = subprocess.call(["git", "clone", "https://github.com/{0}/{1}".format(org, project), tempdir + "/job"])
+    if pr != 'master':
+        subprocess.Popen(["git", "fetch", "origin", "pull/{0}/head:pr_{0}".format(pr)], cwd=(tempdir + "/job")).communicate()
+        subprocess.Popen(["git", "checkout", "pr_{0}".format(pr)], cwd=(tempdir + "/job")).communicate()
+    return str(tempdir)
+
+
+def run_beaker_rspec(tempdir):
+    # record starttime
+    t1 = datetime.datetime.utcnow()
+
+    jobdir = tempdir + "/job"
+    print "running in {0}".format(jobdir)
+
+    # Setup environment
+    runenv = os.environ.copy()
+    runenv["BEAKER_set"] = 'libvirt'
+    runenv["GEM_HOME"] = '/home/pcci/ruby_pcci_gempath'
+    runenv["PATH"] = ':/home/pcci/ruby_pcci_gempath/bin/'
+    print "Using libvirt nodeset"
+
+    # Run the test
+    beaker = subprocess.Popen(["bundle", "exec", "rspec", "spec/acceptance"], cwd=jobdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=runenv)
+    out, err = beaker.communicate()
+
+    # record endtime
+    t2 = datetime.datetime.utcnow()
+
+    #setup response object
+    t_delta = t2 - t1
+    response = { 'gemout'  : gemout,
+                 'gemerr'  : gemerr,
+                 'out'     : out,
+                 'err'     : err,
+                 'success' : int(beaker.returncode),
+                 'time'    : int(t_delta.seconds),
+                 'harness_failure': False,
+                 }
+
+    # really the most important message
+    if response['success'] == 0:
+        print "Tests passed"
+    else:
+        print "Tests failed"
+
+    return response
+
+
+def write_log(work_item, response):
+    path = config.logpath
+    org, project, pr = work_item.split('/')
+    if response['success'] == 0:
+        succ = 'PASS'
+    else:
+        succ = 'FAIL'
+    unix_seconds = datetime.datetime.utcnow().strftime('%s')
+    filename =  "{0}+{1}+{2}+{3}+{4}".format(org,project,pr,unix_seconds,succ)
+    if response['harness_failure']:
+        filename = "harness_failures/" + filename
+
+    with open(path + "/" + filename, 'w') as f:
+        f.write("Test log\n")
+        f.write("Test performed at {0} - {1}\n".format(unix_seconds, datetime.datetime.utcnow()))
+        f.write("{0}/{1} PR # {2}\n".format(org, project, pr))
+        f.write("Took {0} Seconds\n".format(response['time']))
+        if response['success'] == 0:
+            f.write("Tests passed\n")
+        else:
+            f.write("Tests failed\n")
+        for line in response['gemout']:
+            f.write(line)
+        for line in response['gemerr']:
+            f.write(line)
+        for line in response['out']:
+            f.write(line)
+        for line in response['err']:
+            f.write(line)
+    f.closed
+    return (filename)
+
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    setup_worker()
+    main_loop()
+    work_item = json.loads(r.lpop('todo'))
+    if work_item == None:
+        print "No work to do, shutting down"
+        sys.exit()
+    print "starting work on {0}".format(work_item)
+    response = {}
+    tempdir = create_pr_env(work_item['unique_name'])
+    response = run_beaker_rspec(tempdir)
+    log_path = write_log(work_item, response)
+    print "log written to {0}".format(log_path)
+    r.rpush('completed', log_path)
+    print('Shutting down worker')
+    r.decr('workers')
+
+
+
+
+
+
+
+
+
+
